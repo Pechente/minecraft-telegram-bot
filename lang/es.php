2499c6015e1d64b7da436cb16ad5ae8681940bf7
Miguel Arconada <miguelarman@gmail.com>
2024-07-04 20:14:04 +0200

    Added Spanish 🇪🇸 translations

---
+++ lang/es.php
@@ -0,0 +1,9 @@
+<?php
+$lang = [
+  "no_players_connected" => "No hay jugadores conectados :(",
+  "one_player_connected" => "1 jugador conectado.",
+  "players_connected" => "jugadores conectados.",
+  "joined" => "se unió al server.",
+  "disconnected" => "se ha desconectado.",
+];
+?>
